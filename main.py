6387a6bde69964dc7eab9f2ab6648b7626b91ad6
lvvrs <59198795+lvvrs@users.noreply.github.com>
2023-07-27 23:51:13 +0300

    add get requests: 1) get_all_command 2) get_command_by_name

--- main.py
+++ main.py
@@ -12,11 +12,54 @@ class Command(BaseModel):
     command_home_stadium: str
 
 
+command_list = [
+    {
+        "command_name": "Spartak",
+        "command_city": "Moscow",
+        "command_country": "Russia",
+        "command_year_of_foundation": 1922,
+        "command_home_stadium": "Otkritie bank Arena"
+    },
+    {
+        "command_name": "Dinamo",
+        "command_city": "Moscow",
+        "command_country": "Russia",
+        "command_year_of_foundation": 1923,
+        "command_home_stadium": "Dinamo"
+    },
+    {
+        "command_name": "CSKA",
+        "command_city": "Moscow",
+        "command_country": "Russia",
+        "command_year_of_foundation": 1911,
+        "command_home_stadium": "VEB Arena"
+    }
+]
+
+
 @app.get("/")
 async def root():
     return {"Application Name": "Football Club's"}
 
 
+@app.get("/commads/all_commands")
+async def get_all_commands():
+    return command_list
+
+
+@app.get("/commads/{name}")
+async def get_command_by_name(name: str):
+    searched_command = {}
+    for command in command_list:
+        if command["command_name"] == name:
+            searched_command = command
+            break
+    if searched_command == {}:
+        return f"Command with name {name} not found!"
+    return searched_command
+
+
 @app.post("/commands/add", response_model=Command)
 def add_command(command: Command) -> Command:
+    command_list.append(dict(command))
     return command
